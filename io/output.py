621452fac66bc15f39ba70f01534da8d1c0d6820
Tymofii Matiichyk <t.mattw.0000@gmail.com>
2024-03-20 11:53:06 +0200

    Added blank functions to input.py, output.py

---
+++ io/output.py
@@ -0,0 +1,28 @@
+import pandas as pd
+
+
+def printconsole():
+    """
+    Function gets en input from user and returns it
+    :returns: a string object
+    """
+
+    """    
+    prompt = input("Input: ")
+    return prompt
+    """
+
+
+def writefile():
+    """
+    Function reads a file and returns the contents of the file
+    :return: a string object
+    """
+
+
+def writefilepandas():
+    """
+    Function reads a file and returns the content
+    using pandas functionality
+    :return: a string object
+    """
--- main.py
+++ main.py
@@ -7,6 +7,7 @@
 def main():
     pass
 
+
 if __name__ == '__main__':
     main()
-    print("Hello, world!")
+    print("F")
